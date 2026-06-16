16d1df8fc08f427888017ac63a91edbbd4c96b80
RautelaSahil <sahil.rautela.ssr@gmail.com>
2026-06-16 07:26:42 +0000

    Updated the models as per the Phase 2 of task manager

--- tasks/models.py
+++ tasks/models.py
@@ -9,9 +9,42 @@ class Task(models.Model):
     user = models.ForeignKey(User,on_delete = models.CASCADE,null = False)
     def __str__(self):
         return self.title
-    
-class User(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    password_hash = models.CharField(max_length=128)
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
     def __str__(self):
-        return self.username
+        return self.name
+
+class Membership(models.Model):
+    class Roles(models.TextChoices):
+        Leader = 'leader', 'leader'
+        Co_leader = 'co_leader', 'Co-Leader'
+        Elder = 'elder', 'Elder'
+        Member = 'member', 'Member'
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'organization'], name='unique_membership')
+        ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    role = models.CharField(max_length=50, choices=Roles.choices, default = "member")
+
+    def __str__(self):
+        return f"{self.user.username} - {self.organization.name} ({self.role})"
+
+class OrganisationTask(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
+class AssignedTasks(models.Model):
+    task = models.ForeignKey(OrganisationTask, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.task.title} assigned to {self.user.username} in {self.task.organization.name}"
